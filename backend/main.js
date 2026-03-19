d30f4ea8b169e45eaaa860e1e18190064d42691d
Ayush <ayushraut7558@gmail.com>
2026-03-19 21:37:36 +0530

    feat/upload file

--- backend/main.js
+++ backend/main.js
@@ -1,7 +1,24 @@
 require('dotenv').config()
 const express = require('express');
+const multer = require('multer');
 const app = express(); 
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); // folder must exist
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    console.log("Hello")
+  console.log(req.file); // file info
+  res.send("File uploaded successfully");
+});
 
 app.get('/server-health', (req, res) => {
     res.send(`Server is Healthy`);
